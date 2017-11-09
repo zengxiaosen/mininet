8d7885da652cf7e4848d6cb351e2e97a6e8f6b86
zengxiaosen <919451814@qq.com>
2017-11-09 21:02:19 +0800

    test

--- mininet/cli.py
+++ mininet/cli.py
@@ -210,6 +210,22 @@ class CLI( Cmd ):
         "Ping between first two hosts, returns all ping results."
         self.mn.pingPairFull()
 
+
+    def do_iperfmulti( self, line ):
+        """Multi iperf UDP test between nodes"""
+        args = line.split()
+        if len(args) == 1:
+            udpBw = args[ 0 ]
+            self.mn.iperfMulti(udpBw)
+        elif len(args) == 2:
+            udpBw = args[ 0 ]
+            period = args[ 1 ]
+            err = False
+            self.mn.iperfMulti(udpBw, float(period))
+        else:
+            error('invalid number of args: iperfmulti udpBw \n' +
+                   'udpBw examples: 1M\n')
+
     def do_iperf( self, line ):
         """Simple iperf TCP test between two (optionally specified) hosts.
            Usage: iperf node1 node2"""
