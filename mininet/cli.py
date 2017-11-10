2c9d8fcf3b4072e8c5cabfa1c72286330475cae8
zengxiaosen <919451814@qq.com>
2017-11-10 10:59:39 +0800

    全链接地打流量

--- mininet/cli.py
+++ mininet/cli.py
@@ -226,6 +226,21 @@ class CLI( Cmd ):
             error('invalid number of args: iperfmulti udpBw \n' +
                    'udpBw examples: 1M\n')
 
+    # def do_iperfmulti( self, line ):
+    #     """Multi iperf UDP test between nodes"""
+    #     args = line.split()
+    #     if len(args) == 1:
+    #         udpBw = args[ 0 ]
+    #         self.mn.iperfMulti(udpBw)
+    #     elif len(args) == 2:
+    #         udpBw = args[ 0 ]
+    #         period = args[ 1 ]
+    #         err = False
+    #         self.mn.iperfMulti(udpBw, float(period))
+    #     else:
+    #         error('invalid number of args: iperfmulti udpBw \n' +
+    #                'udpBw examples: 1M\n')
+
     def do_iperf( self, line ):
         """Simple iperf TCP test between two (optionally specified) hosts.
            Usage: iperf node1 node2"""
--- mininet/net.py
+++ mininet/net.py
@@ -788,7 +788,7 @@ class Mininet( object ):
             return ''
 
 
-    def iperf_single( self, hosts=None, udpBw='10M', period=10, port=5001):
+    def iperf_single( self, hosts=None, udpBw='10M', period=5, port=5001):
         """Run iperf between two hosts using UDP.
              hosts: list of hosts; if None, uses opposite hosts
              returns: results two-element array of server and client speeds"""
@@ -803,11 +803,56 @@ class Mininet( object ):
         iperfArgs = 'iperf -u '
         bwArgs = '-b ' + udpBw + ' '
         print "***start server***"
-        server.cmd( iperfArgs + '-s' + ' > /home/zengxiaosen/log/' + 'server' + filename + '&')
+        server.cmd( iperfArgs + '-s' + ' >> /home/zengxiaosen/log/' + 'server' + filename + '&')
         print "***start client***"
         client.cmd(
             iperfArgs + '-t '+ str(period) + ' -c ' + server.IP() + ' ' + bwArgs
-            +' > /home/muzi/log/' + 'client' + filename +'&')
+            +' >> /home/zengxiaosen/log/' + 'client' + filename + '&')
+
+    # def iperf_single( self,hosts=None, udpBw='10M', period=5, port=5001):
+    #     """Run iperf between two hosts using UDP.
+    #        hosts: list of hosts; if None, uses opposite hosts
+    #        returns: results two-element array of server and client speeds"""
+    #     if not hosts:
+    #         return
+    #     else:
+    #         assert len( hosts ) == 2
+    #     client, server = hosts
+    #     filename = client.name[1:] + '.out'
+    #     output( '*** Iperf: testing bandwidth between ' )
+    #     output( "%s and %s\n" % ( client.name, server.name ) )
+    #     iperfArgs = 'iperf -u '
+    #     bwArgs = '-b ' + udpBw + ' '
+    #     print "***start server***"
+    #     server.cmd( iperfArgs + '-s' + ' > /home/zengxiaosen/log/' + filename + '&')
+    #     print "***start client***"
+    #     client.cmd(
+    #         iperfArgs + '-t '+ str(period) + ' -c ' + server.IP() + ' ' + bwArgs
+    #         +' > /home/zengxiaosen/log/' + 'client' + filename +'&')
+
+    # def iperfMulti(self, bw, period=5):
+    #     base_port = 5001
+    #     server_list = []
+    #     client_list = [h for h in self.hosts]
+    #     host_list = []
+    #     host_list = [h for h in self.hosts]
+     
+    #     cli_outs = []
+    #     ser_outs = []
+     
+    #     _len = len(host_list)
+    #     for i in xrange(0, _len):
+    #         client = host_list[i]
+    #         server = client
+    #         while( server == client ):
+    #             server = random.choice(host_list) 
+    #         server_list.append(server)
+    #         self.iperf_single(hosts = [client, server], udpBw=bw, period= period, port=base_port)
+    #         sleep(.05)
+    #         base_port += 1
+    #     self.hosts[0].cmd('ping -c10'+ self.hosts[-1].IP() + ' >> /home/zengxiaosen/log/delay.out')
+    #     sleep(period)
+
 
     def iperfMulti(self, bw, period=5):
       base_port = 5001
@@ -830,17 +875,7 @@ class Mininet( object ):
             self.iperf_single(hosts = [client, server], udpBw=bw, period=period, port=base_port)
             sleep(.05)
         base_port += 1
-
-      for i in xrange(0, _len):
-        client = host_list[i]
-        for j in xrange(0, _len):
-          server = host_list[j]
-          if server == client:
-            continue
-          else:
-            client.cmd('ping -c10'+ server.IP() + ' >> /home/muzi/log/delay.out')
-            sleep(.05)
-        base_port += 1
+      self.hosts[0].cmd('ping -c10'+ self.hosts[-1].IP() + ' >> /home/zengxiaosen/log/delay.out')
       sleep(period)
 
 
