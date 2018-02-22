bd8186deee142988dbec6baeefbd5fcf6f328a54
zengxiaosen <919451814@qq.com>
2018-02-23 00:44:03 +0800

    test

--- custom/fattree.py
+++ custom/fattree.py
@@ -135,7 +135,7 @@ def pingTest(net):
     logger.debug("Start Test all network")
     net.pingAll()
 
-
+#10.108.37.197
 def createTopo(pod, density, ip="127.0.0.1", port=6633, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.05):
     logging.debug("LV1 Create Fattree")
     topo = Fattree(pod, density)
--- mininet/net.py
+++ mininet/net.py
@@ -788,27 +788,25 @@ class Mininet( object ):
             return ''
 
 
-    def iperf_single( self, hosts=None, udpBw='10M', period=10, port=5001):
+    def iperf_single( self,hosts=None, udpBw='10M', period=60, port=5001):
         """Run iperf between two hosts using UDP.
-             hosts: list of hosts; if None, uses opposite hosts
-             returns: results two-element array of server and client speeds"""
+           hosts: list of hosts; if None, uses opposite hosts
+           returns: results two-element array of server and client speeds"""
         if not hosts:
-          return
+            return
         else:
-          assert len( hosts ) == 2
+            assert len( hosts ) == 2
         client, server = hosts
-        filename = client.name[1:] + ".out"
+        filename = client.name[1:] + '.out'
         output( '*** Iperf: testing bandwidth between ' )
         output( "%s and %s\n" % ( client.name, server.name ) )
         iperfArgs = 'iperf -u '
         bwArgs = '-b ' + udpBw + ' '
         print "***start server***"
-        # server.cmd( iperfArgs + '-s' + ' >> /home/zengxiaosen/log/' + 'server' + filename + '&')
-        server.cmd( iperfArgs + '-s' + ' >> /home/zengxiaosen/log/' + 'server' + filename + '&')
+        server.cmd( iperfArgs + '-s -i 1' + ' > /home/zengxiaosen/log/' + filename + '&')
         print "***start client***"
         client.cmd(
-            iperfArgs + '-t '+ str(period) + ' -c ' + server.IP() + ' ' + bwArgs
-            +' >> /home/zengxiaosen/log/' + 'client' + filename + '&')
+            iperfArgs + '-t '+ str(period) + ' -c ' + server.IP() + ' ' + bwArgs +' > /home/zengxiaosen/log/' + 'client' + filename +'&')
 
     # def iperf_single( self,hosts=None, udpBw='10M', period=5, port=5001):
     #     """Run iperf between two hosts using UDP.
@@ -855,42 +853,31 @@ class Mininet( object ):
     #     sleep(period)
 
 
-    def iperfMulti(self, bw, period=10):      
-      base_port = 5001
-      server_list = [h for h in self.hosts]
-      client_list = [h for h in self.hosts]
-      host_list = []
-      host_list = [h for h in self.hosts]
-
-      cli_outs = []
-      ser_outs = []
-
-      _len = len(host_list)
-      for kkk in xrange(0, 20):
-        for i in xrange(0, _len):
-          client = host_list[i]
-          for j in xrange(0, _len):
-            server = host_list[j]
-            if server == client:
-              continue
-            else:
-              self.iperf_single(hosts = [client, server], udpBw=bw, period=period, port=base_port)
-              sleep(.05)
-          base_port += 1
+    def iperfMulti(self, bw, period=60):      
         base_port = 5001
+        # server_list = [h for h in self.hosts]
+        server_list = []
+        client_list = [h for h in self.hosts]
+        host_list = []
+        host_list = [h for h in self.hosts]
 
+        cli_outs = []
+        ser_outs = []
 
-      self.hosts[0].cmd('ping -c10'+ self.hosts[-1].IP() + ' >> /home/zengxiaosen/log/delay.out')
-      #sleep(period)
-
-
- 
-
-
-
-
-
-
+        _len = len(host_list)
+        for i in xrange(0, _len):
+            client = host_list[i]
+            server = client
+            while( server == client ):
+                server = random.choice(host_list) 
+            server_list.append(server)
+            self.iperf_single(hosts = [client, server], udpBw=bw, period= period, port=base_port)
+            #self.iperf(hosts = [client, server], udpBw=bw, seconds=period, port=base_port)
+            sleep(.05)
+            base_port += 1
+        self.hosts[0].cmd('ping -c10'+ self.hosts[-1].IP() + ' > /home/zengxiaosen/log/delay.out')
+        sleep(period)
+        print "test has done"
 
 
 
